f16f6238364c91adbeceeba8ecc56cd34f4df053
Celalyvan <tomasaldinio@hotmail.com>
2021-06-16 15:43:59 -0300

    adicion de efectos de sonido + lectura de Json <To++>

--- HelloWorld-Box2d/core/src/com/helloworld/box2dprueba/states/ScoreState.java
+++ HelloWorld-Box2d/core/src/com/helloworld/box2dprueba/states/ScoreState.java
@@ -3,6 +3,11 @@ package com.helloworld.box2dprueba.states;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ScoreState extends State{
 
@@ -25,5 +30,36 @@ public class ScoreState extends State{
     public void dispose(){}
 
     //metodos para levantar los score del archivo
+    public ArrayList<Score> jsonToArray(){
+        ArrayList<Score> localArray = new ArrayList<>();
+        File file = new File("score.json");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
 
+            Gson localGson = new Gson();
+
+            while (localArray.add(localGson.fromJson(reader, Score.class))!=null);
+
+
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace(System.out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        return localArray=top10Scores(localArray);
+    }
+
+    protected ArrayList<Score> top10Scores(ArrayList<Score> totalScores){
+        Collections.sort(totalScores);
+
+        for(int i = 10; i < totalScores.size(); i++) {
+            if(totalScores.remove(i)==null){break;}
+        }
+
+        return totalScores;
+    }
 }
