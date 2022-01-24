a8fc5fb2bc3408e9aea773007f9f0b88fc802cf6
elomdq <eloymrp@gmail.com>
2022-01-24 13:34:00 -0300

    correcion url en import de GameApp - Eloy

--- Game/desktop/src/com/helloworld/box2dprueba/desktop/DesktopLauncher.java
+++ Game/desktop/src/com/helloworld/box2dprueba/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.helloworld.box2dprueba.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.helloworld.box2dprueba.GameApp;
+import org.GameApp;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
